5fa03569d3473fc8ecfc68883134b2fbebf2b6dd
Kevin Chandler <im.kevin@me.com>
2015-01-01 23:06:50 -0800

    subscribe route non-api

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,33 +1,36 @@
    
-  function subscribe() {
-    var emailRe = /^([a-z][a-z0-9\*\-\.]*):\/\/(?:(?:(?:[\w\.\-\+!$&'\(\)*\+,;=]|%[0-9a-f]{2})+:)*(?:[\w\.\-\+%!$&'\(\)*\+,;=]|%[0-9a-f]{2})+@)?(?:(?:[a-z0-9\-\.]|%[0-9a-f]{2})+|(?:\[(?:[0-9a-f]{0,4}:)*(?:[0-9a-f]{0,4})\]))(?::[0-9]+)?(?:[\/|\?](?:[\w#!:\.\?\+=&@!$'~*,;\/\(\)\[\]\-]|%[0-9a-f]{2})*)?$/
+  // function subscribe() {
     
-    var webhookUrl = emailRe.exec($('#webhookUrl').val());
+  // }
+
+  // $(document).ready(function() {
+  //   var subscribeBtn = document.getElementById('subscribeBtn');
+  //   subscribeBtn.addEventListener('click', function() {
+  //     var emailRe = /^([a-z][a-z0-9\*\-\.]*):\/\/(?:(?:(?:[\w\.\-\+!$&'\(\)*\+,;=]|%[0-9a-f]{2})+:)*(?:[\w\.\-\+%!$&'\(\)*\+,;=]|%[0-9a-f]{2})+@)?(?:(?:[a-z0-9\-\.]|%[0-9a-f]{2})+|(?:\[(?:[0-9a-f]{0,4}:)*(?:[0-9a-f]{0,4})\]))(?::[0-9]+)?(?:[\/|\?](?:[\w#!:\.\?\+=&@!$'~*,;\/\(\)\[\]\-]|%[0-9a-f]{2})*)?$/
     
-    if (webhookUrl) {
-      webhookUrl = webhookUrl[0].toLowerCase()
+  //     var webhookUrl = emailRe.exec($('#webhookUrl').val());
+      
+  //     if (webhookUrl) {
+  //       webhookUrl = webhookUrl[0].toLowerCase()
 
-        $.ajax({
-          url: '/api/subscribe',
-          method: 'POST',
-          data: {
-            webhookUrl: webhookUrl
-          }
-        })
-        .done(function( data ) {
-          $('section#subscribe').hide();
-          $('section#success').show();
-      });
-    }
-    else {
-      alert('invalid webhook url')
-    }
-  }
+  //         $.ajax({
+  //           url: '/api/subscribe',
+  //           method: 'POST',
+  //           data: {
+  //             webhookUrl: webhookUrl
+  //           }
+  //         })
+  //         .done(function( data ) {
+  //           $('#subscribe').hide();
+  //           alert('success!')
+  //       });
+  //     }
+  //     else {
+  //       alert('invalid webhook url')
+  //     }
 
-  $(document).ready(function() {
-    var subscribeBtn = document.getElementById('subscribeBtn');
-    subscribeBtn.addEventListener('click', function() {
-      subscribe();
-    })
-  })
+
+      
+  //   })
+  // })
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,65 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var db = require('../lib/db.js');
+
+
 router.get('/', function(req, res) {
   res.render('index', { title: 'KittehMail' });
 });
+
+
+router.post('/subscribe', function(req, res) {
+	var emailRe = /^([a-z][a-z0-9\*\-\.]*):\/\/(?:(?:(?:[\w\.\-\+!$&'\(\)*\+,;=]|%[0-9a-f]{2})+:)*(?:[\w\.\-\+%!$&'\(\)*\+,;=]|%[0-9a-f]{2})+@)?(?:(?:[a-z0-9\-\.]|%[0-9a-f]{2})+|(?:\[(?:[0-9a-f]{0,4}:)*(?:[0-9a-f]{0,4})\]))(?::[0-9]+)?(?:[\/|\?](?:[\w#!:\.\?\+=&@!$'~*,;\/\(\)\[\]\-]|%[0-9a-f]{2})*)?$/
+  	var webhookUrl = emailRe.exec(req.body.webhookUrl)
+
+  	if ( webhookUrl ) {
+		
+		var params = {
+			webhookUrl: webhookUrl[0],
+			ip: req.headers['X-Real-IP'] || req.connection.remoteAddress
+		};
+
+		// check to make sure isn't already subscribed
+		db.findEntry( { webhookUrl: params.webhookUrl }, function(err, entry) {
+
+			if ( err ) {
+				console.log('db error');
+				// return res.status(500).end()
+				return res.render('index', { message: err })
+				
+			}
+
+			else if ( entry ) {
+				// return res.status(204).end()
+				return res.render('index', { message: 'Success!'})
+			}
+
+			else if ( !entry ) {
+				console.log('1');
+				db.subscribe(params, function(err, succ) {
+					if ( err ) {
+						console.log('err');
+						console.log(err);
+						// return res.status(500).end()
+						return res.render('index', { message: err })
+					}
+					else if ( succ ) {
+						console.log('2');
+						console.log('subscribed: ' + succ);
+						// return res.status(200).end()
+						return res.render('index', { message: 'Success!', success: true })
+					}
+					console.log('3');
+				})
+
+			}
+		})
+	}
+	else {
+		// res.status(500).end();
+		return res.render('index', { message: 'invalid webhook url' });
+	}
+})
 
 module.exports = router;
